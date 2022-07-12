43ca1c3f19dfbe370c9ab11b83f25718f1e33238
dongil618 <rlaehddlf618@gmail.com>
2022-07-13 01:07:51 +0900

    CREATE files - First Commit

---
+++ src/main/java/com/hanghae99/myblog/MyblogApplication.java
@@ -0,0 +1,15 @@
+package com.hanghae99.myblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class MyblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyblogApplication.class, args);
+    }
+
+}
